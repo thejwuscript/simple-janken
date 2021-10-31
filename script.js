679eb32869bf9f9f5cd438a2a9f11aec027587e2
James Wu <ichangjwu@outlook.com>
2021-10-31 12:36:05 +0900

    reworked the game with buttons and displayed alerts as text

--- script.js
+++ script.js
@@ -10,8 +10,7 @@ function computerPlay() {
 
 }
 
-let computerSelection = computerPlay();
-let playerSelection;
+
 let num2;
 let roundcount = 1;
 let playerscore = 0;
@@ -23,104 +22,77 @@ function tryagain() {
   num2 = 0;
 }
 
-function playRound(playerSelection, computerSelection) { 
+function playRound(e) { 
   
-  do {
-    initalplay = prompt(`Round ${roundcount++}.  Rock, Paper or Scissors?`, "");
-    playerSelection = initalplay.toUpperCase().trim();
-    if (playerSelection == "ROCK") {
-    num2 = 1
-    } else if (playerSelection == "PAPER") {
-    num2 = 1 
-    } else if (playerSelection == "SCISSORS") {
-    num2 = 1
-    } else tryagain();
-  } while (num2 == 0);
+  let playerSelection = e.target.className;
+  let computerSelection = computerPlay();
+  let div1 = document.querySelector("div.display-results");
+  
+  
 
    switch (playerSelection.concat(computerSelection)) {
     case "ROCKROCK":
-      alert("Computer played rock. It's a tie!");
-      ++playerscore;
-      ++compscore;
+      div1.textContent = "Computer played rock. It's a tie!";
+      div1.style.cssText = "vertical-align: middle; line-height: 50px";
       break;
     case "PAPERPAPER":
-      alert("Computer played paper. It's a tie!");
-      ++playerscore;
-      ++compscore;
+      div1.textContent = "Computer played paper. It's a tie!";
+      div1.style.cssText = "vertical-align: middle; line-height: 50px";
       break;
     case "SCISSORSSCISSORS":
-      alert("Computer played scissors. It's a tie!");
-      ++playerscore;
-      ++compscore;
-       break;
+      div1.textContent = "Computer played scissors. It's a tie!";
+      div1.style.cssText = "vertical-align: middle; line-height: 50px";
+      break;
     case "ROCKSCISSORS":
-      alert("Computer played scissors. You win this round!");
-      playerscore += 2;
+      div1.textContent = "Computer played scissors. You win this round!";
+      div1.style.cssText = "vertical-align: middle; line-height: 50px";
+      playerscore ++;
       break;
     case "PAPERROCK":
-      alert("Computer played rock. You win this round!");
-      playerscore += 2;
+      div1.textContent = "Computer played rock. You win this round!";
+      div1.style.cssText = "vertical-align: middle; line-height: 50px";
+      playerscore ++;
       break;
     case "SCISSORSPAPER":
-      alert("Computer played paper. You win this round!");
-      playerscore += 2;
+      div1.textContent = "Computer played paper. You win this round!";
+      div1.style.cssText = "vertical-align: middle; line-height: 50px";
+      playerscore ++;
       break;
     case "ROCKPAPER":
-      alert("Computer played paper. Oh no.");
-      compscore += 2;
+      div1.textContent = "Computer played paper. Oh no.";
+      div1.style.cssText = "vertical-align: middle; line-height: 50px";
+      compscore ++;
       break;
     case "PAPERSCISSORS":
-      alert("Computer played scissors. Oh no.");
-      compscore += 2;
+      div1.textContent = "Computer played scissors. Oh no.";
+      div1.style.cssText = "vertical-align: middle; line-height: 50px";
+      compscore ++;
       break;
     case "SCISSORSROCK":
-      alert("Computer played rock. Oh no.");
-      compscore += 2;
+      div1.textContent = "Computer played rock. Oh no.";
+      div1.style.cssText = "vertical-align: middle; line-height: 50px";
+      compscore ++;
       break;
 
   }
-}
 
-function game() {
-  playRound(playerSelection, computerSelection);
-  computerPlay();
-  computerSelection = computerPlay();
-  playRound(playerSelection, computerSelection);
-  computerPlay();
-  computerSelection = computerPlay();
-  playRound(playerSelection, computerSelection);
-  computerPlay();
-  computerSelection = computerPlay();
-  playRound(playerSelection, computerSelection);
-  computerPlay();
-  computerSelection = computerPlay();
-  playRound(playerSelection, computerSelection);
-}
+  div2.textContent = playerscore + "     " + compscore;
 
-alert("Let's play Janken!\nThere are 5 rounds against the computer.");
-alert("Rules:\nWin - 2 points\nTie - 1 point\nLose - 0 points")
-alert("Points will be tallied after 5 rounds.\nGame START!")
-
-game();
-
-alert(`Game END.\nYour score is ${playerscore}.\nComputer's score is ${compscore}.`)
-
-if (playerscore > compscore) {
-  alert("Congratulations! You won the game!");  
-} else if (playerscore < compscore) {
-  alert("You lost the game.");
-} else if (playerscore == compscore) {
-  alert("Wow! It's a draw! Tiebreaker round!");
-  do {
-    computerPlay();
-    playRound(playerSelection, computerSelection);
+  if (playerscore == 5) {
+    div3.textContent = "Congratulations! You won the game!";
+  } else if (compscore == 5) {
+    div3.textContent = "Oh no. You lost the game :-(";
   }
-  while (playerscore == compscore);
-  if (playerscore > compscore) {
-    alert("Tie broken! You've won the game!")
-  } else if (playerscore < compscore) {
-    alert("Oh no, so close! You've lost the game : (");
+  
+
   }
-}
 
 
+const btn = document.querySelectorAll("button");
+const div2 = document.querySelector("div.display-score");
+const div3 = document.querySelector("div.winner");
+
+
+btn.forEach( function(elem) {
+  elem.addEventListener("click", playRound);
+});
